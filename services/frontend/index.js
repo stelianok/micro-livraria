727989e8b32b24b3756507123cabccb87537bd05
Kauã Steliano <stelianok@gmail.com>
2025-03-31 09:40:24 -0300

    Implemented frontend

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -15,7 +15,7 @@ function newBook(book) {
             <div class="card-content">
                 <div class="content book" data-id="${book.id}">
                     <div class="book-meta">
-                        <p class="is-size-4">R$${book.price.toFixed(2)}</p>
+                        <p class="is-size-4">R${book.price.toFixed(2)}</p>
                         <p class="is-size-6">Disponível em estoque: 5</p>
                         <h4 class="is-size-3 title">${book.name}</h4>
                         <p class="subtitle">${book.author}</p>
@@ -29,10 +29,29 @@ function newBook(book) {
                         </div>
                     </div>
                     <button class="button button-buy is-success is-fullwidth">Comprar</button>
+                    <button class="button search-product is-fullwidth">Mais informações</button>
                 </div>
             </div>
         </div>`;
     return div;
+}
+
+function getDetailedProductData(id) {
+    fetch(`http://localhost:3000/product/${id}`)
+        .then((data) => {
+            if (data.ok) {
+                console.log(data);
+                return data.json();
+            }
+            throw data.statusText;
+        })
+        .then((productData) => {
+            window.location.href = `http://localhost:3000/product/${id}`;
+        })
+        .catch((err) => {
+            swal('Erro', 'Erro ao consultar listar dados', 'error');
+            console.error(err);
+        });
 }
 
 function calculateShipping(id, cep) {
@@ -44,7 +63,7 @@ function calculateShipping(id, cep) {
             throw data.statusText;
         })
         .then((data) => {
-            swal('Frete', `O frete é: R$${data.value.toFixed(2)}`, 'success');
+            swal('Frete', `O frete é: R${data.value.toFixed(2)}`, 'success');
         })
         .catch((err) => {
             swal('Erro', 'Erro ao consultar frete', 'error');
@@ -73,6 +92,13 @@ document.addEventListener('DOMContentLoaded', function () {
                         const id = e.target.getAttribute('data-id');
                         const cep = document.querySelector(`.book[data-id="${id}"] input`).value;
                         calculateShipping(id, cep);
+                    });
+                });
+
+                document.querySelectorAll('.search-product').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        const id = e.target.closest('.content.book').getAttribute('data-id');
+                        getDetailedProductData(id);
                     });
                 });
 
